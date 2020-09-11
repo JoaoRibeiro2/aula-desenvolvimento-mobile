a6fa8be286481696da119c8d7ba2a42fec64c6c9
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-09-11 11:51:25 +0900

    APi

--- www/components/script.js
+++ www/components/script.js
@@ -1,16 +1,64 @@
-$(document).on("click","#previsao",function(){
-  $.ajax({
-    type: "get", // Como vou enviar
-    //url: "https://viacep.com.br/ws/"+$("#cep").val()+"/json/",
-    url: "https://api.hgbrasil.com/weather?key=9a5da2c2",
-    success: function(data){ // Executar se der certo
-      var previsao = ({
-             "country": data.date
-         })
-      $("#dados").html(previsao);
-    },
-    error: function(){
-      alert("Deu ruim");
-    }
-  })
+
+
+
+
+let cidade = document.querySelector("#cidade")
+let uf = document.querySelector("#estado")
+let search = document.querySelector("#buscar")
+let info = document.querySelector("#info")
+
+
+let cond  = document.querySelector("#condicao")
+let data = document.querySelector("#date")
+let desc = document.querySelector("#description")
+let maxima = document.querySelector("#max")
+let minima = document.querySelector("#min")
+let dia_semana = document.querySelector("#weekday")
+
+uf.addEventListener("blur" ,(e) => {
+	let cep = cidade.value
+
+	const options = {
+		method: 'GET',
+		mode: 'cors',
+		cache: 'default'  
+	}
+
+	// fetch(`https://viacep.com.br/ws/${cep}/json/`, options)
+	fetch(`https://api.hgbrasil.com/weather?key=8c5d4364&city_name=${cep},${estado}&format=json-cors`, options)
+
+	
+	.then((response) => {response.json()
+		.then( data => showData(data.results.forecast))
+	})
+	.catch(e => console.log('Deu erro: '+ e,message))
+
 })
+
+
+const showData = (result) => {
+		
+		// for(const campo in result){
+		// 	console.log(campo[1])
+		// }
+// const campo = result[0]
+
+	for(i=1; i<7; i++){
+
+		let previsao = document.createElement("div")
+		previsao.innerHTML = `
+
+			<H2>Data: ${result[i].date} </H2>
+			<h3>Dia Semana: ${result[i].weekday} </label></h3>
+			<p>Descrição do tempo: <label id="description"> ${result[i].description} </label></p>
+			<p>Max: <label id="max"> ${result[i].max} °C </label></p>
+			<p>Min: <label id="min"> ${result[i].min} °C</label></p>
+		`
+		info.appendChild(previsao)
+		console.log(previsao)
+	}
+
+
+
+// console.log(campo)
+}
